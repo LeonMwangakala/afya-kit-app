ac8df19e5c8b9dae0c2ba146543b13bd619bec69
LeonMwangakala <ljmwangakala@yahoo.com>
2016-02-07 22:36:39 +0800

    Development

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,46 @@
+class PatientsController < ApplicationController
+   before_action :set_patient, only: [:edit, :update, :show, :destroy]
+   def index
+       @patients = Patient.all
+   end
+   def new
+       @patient = Patient.new
+   end
+   
+   def create
+      @patients = Patient.all
+      @patient = Patient.create(patient_params) # whitelisting new patients
+      #flash[:success] = "Patient was successfully registered"
+   end
+   
+   def show
+
+   end
+   
+   def edit
+
+   end
+   
+   def update
+      @patients = Patient.all
+      @patient.update_attributes(patient_params)
+      #flash[:success] = "Patient information was successfully updated"
+   end
+   
+   
+   def destroy
+      @patients = Patient.all
+      @patient.destroy
+      #flash[:info] = "Patient was successfully deleted"
+   end
+   
+   
+   private 
+       def set_patient
+          @patient = Patient.find(params[:id]) 
+       end
+       def patient_params
+           params.require(:patient).permit(:first_name, :last_name, :date_of_birth, :region)
+       end
+       
+end
