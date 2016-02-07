d6061b4bb186b8f7eaca85949649d17461549595
LeonMwangakala <ljmwangakala@yahoo.com>
2016-02-08 02:40:44 +0800

    Remove associations patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,9 +1,8 @@
 class Patient < ActiveRecord::Base
-    has_many :appointments
-    has_many :users, through: :appointments
-    has_many :appointment_slots, through: :appointments
+    
     validates :first_name, presence: true, length: {minimum: 3, maximum: 20} 
     validates :last_name, presence: true, length: {minimum: 3, maximum: 20} 
-    validates :date_of_birth, presence: true, format: {with: /[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}/, message: "must be DD/MM/YYYY"}
+    validates :date_of_birth, presence: true, 
+              format: {with: /[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}/, message: "must be DD/MM/YYYY"}
     validates :region, presence: true, length: {minimum: 3, maximum: 15}
 end
