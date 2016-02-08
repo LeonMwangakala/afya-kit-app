2be0f100fcb9461363ff859d3e79fc7dadbd6d44
LeonMwangakala <ljmwangakala@yahoo.com>
2016-02-08 22:10:06 +0800

    Add validations to doctor model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,6 @@
 class Doctor < ActiveRecord::Base
   has_one :user, as: :profile
+
+  validates :first_name, presence: true, length: {minimum: 3, maximum: 20} 
+  validates :last_name, presence: true, length: {minimum: 3, maximum: 20} 
 end
