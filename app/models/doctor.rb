04154f6140075258e5a34fd8c2da8964bf01da8b
LeonMwangakala <ljmwangakala@yahoo.com>
2016-02-09 12:15:25 +0800

    Add nested_attributes

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,5 +1,6 @@
 class Doctor < ActiveRecord::Base
   has_one :user, as: :profile, dependent: :destory
+  accepts_nested_attributes_for :user
 
   validates :first_name, presence: true, length: {minimum: 3, maximum: 20} 
   validates :last_name, presence: true, length: {minimum: 3, maximum: 20} 
