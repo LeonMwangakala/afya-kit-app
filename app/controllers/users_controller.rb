2da4b7c94a15cfe8623af9d7c74035c7266a5484
LeonMwangakala <ljmwangakala@yahoo.com>
2016-03-18 00:14:00 +0800

    Add edit and update functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+
   def index
 
   end
@@ -11,13 +13,29 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
   end
 
+  def edit
+    
+  end
+
+  def update
+    if @user.update(user_params) 
+        flash[:info] = "Your account was successfully updated"
+        redirect_to users_path
+    else
+       render 'edit'  
+    end
+  end
+
   def show
-    @user = User.find(params[:id])
+
   end
 
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
   def user_params
-    params.require(:doctor).permit! 
+    params.require(:user).permit! 
   end
   
 end
