004655b3cea17c4e17eb55851bd715df25f2ff7d
LeonMwangakala <ljmwangakala@yahoo.com>
2016-03-19 17:05:58 +0800

    Add edit, update and show functionality for doctor and patient

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,4 +1,6 @@
 class DoctorsController < ApplicationController
+  before_action :set_doctor, only: [:edit, :update, :show]
+
   def new
     @doctor = Doctor.new
     @user = User.new
@@ -8,11 +10,28 @@ class DoctorsController < ApplicationController
     @doctor = Doctor.create(doctor_params)
   end
 
+  def edit
+    
+  end
+
+  def update
+    if @doctor.update(doctor_params) 
+        flash[:info] = "Your account was successfully updated"
+        redirect_to doctors_path
+    else
+       render 'edit'  
+    end
+  end
+
   def show
-    @doctor = Doctor.find(params[:id])
+    
   end
 
   private
+  def set_doctor
+    @doctor = Doctor.find(params[:id])
+  end
+
   def doctor_params
     params.require(:doctor).permit!
   end
